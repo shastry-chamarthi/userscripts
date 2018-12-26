9d0cb0a94d28707cb564b99afe133580928c9ae1
Sathyanarayana Shastry Chamarthi <sathyanarayanasastry@gmail.com>
2018-12-26 12:15:31 -0600

    Clean ads on eenadu.net
    Cleans ads, removes images and enables reader mode. Good for those who can read telugu and looking for Telangana / Andhra pradesh news

---
+++ eenadu.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Eenadu banner ads
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Clean eenadu.net site and read peacefully
+// @author       You
+// @match        http://*.eenadu.net/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    var elem = document.getElementById('body_ad');
+    elem = (elem) ? elem.remove() : '';
+    
+    //var style = document.createElement('script');
+    var styles = " #body_ad,.socio, .GoogleActiveViewClass, img, .ads680-30, .ad-block-300, .lcol-ad-block, .ytp-thumbnail-overlay, .videoplay,footer, header, .innertop, .two-col-left-block img  { display: none !important; opacity: 0 !important; visibility: none !important; position:absolute !important; left: -999em !important;}";
+    styles += ".col-left, .col-right, #wrapper, .gridContainer, .  { width : 100% !important }";
+    styles += ".thumb-description, .article-title-rgt { float: none !important; width: auto !important; } ";
+    styles += ".box-shadow { box-shadow : none !important; }";
+    jQuery('<style type="text/css" />').append(styles).appendTo($('body'));
+})();
